c8a36578644d5b4f1a11b59fc3e350bf41ab7e1c
Adam Campbell <adam@hotmeteor.com>
2019-09-16 16:43:53 -0500

    Add response to CouldNotSendNotification exception

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,10 +2,44 @@
 
 namespace NotificationChannels\Webhook\Exceptions;
 
+use GuzzleHttp\Psr7\Response;
+
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($response)
+    private $response;
+
+    /**
+     * CouldNotSendNotification constructor.
+     * @param Response $response
+     * @param string $message
+     * @param int|null $code
+     */
+    public function __construct(Response $response, string $message, int $code = null)
     {
-        return new static('Webhook responded with an error: `'.$response->getBody()->getContents().'`');
+        $this->response = $response;
+        $this->message = $message;
+        $this->code = $code ?? $response->getStatusCode();
+
+        parent::__construct($message, $code);
+    }
+
+    /**
+     * @param Response $response
+     * @return CouldNotSendNotification
+     */
+    public static function serviceRespondedWithAnError(Response $response)
+    {
+        return new self(
+            $response,
+            sprintf('Webhook responded with an error: `%s`', $response->getBody()->getContents())
+        );
+    }
+
+    /**
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
     }
 }
